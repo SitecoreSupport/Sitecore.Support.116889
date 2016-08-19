8d7456cb2f5ef89558e52dd751e80ddb57304fb4
Oleg Burov <oleg.burov@outlook.com>
2016-08-18 20:11:28 -0700

    Implemented patch.

---
+++ src/Sitecore.Support.116889/Links/LinkProvider.cs
@@ -0,0 +1,95 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.Links;
+using Sitecore.Sites;
+using Sitecore.Web;
+using System;
+
+namespace Sitecore.Support.Links
+{
+  public class LinkProvider : Sitecore.Links.LinkProvider
+  {
+    #region Public Methods
+
+    public override string GetItemUrl(Item item, UrlOptions options)
+    {
+      Assert.ArgumentNotNull(item, "item");
+      Assert.ArgumentNotNull(options, "options");
+
+      this.ResolveLanguage(item, options);
+
+      var itemUrl = CreateLinkBuilder(options).GetItemUrl(item);
+      if (options.LowercaseUrls)
+      {
+        itemUrl = itemUrl.ToLowerInvariant();
+      }
+      return itemUrl;
+    }
+
+    #endregion Public Methods
+    
+    #region Private Methods
+
+    private void ResolveLanguage(Item item, UrlOptions defaultUrlOptions)
+    {
+      Assert.ArgumentNotNull(item, "item");
+      Assert.ArgumentNotNull(defaultUrlOptions, "defaultUrlOptions");
+
+      if (!Settings.Rendering.SiteResolving)
+      {
+        return;
+      }
+
+      if (item != null)
+      {
+        if (MatchCurrentSite(item, new SiteContext(Context.Site.SiteInfo)))
+        {
+          defaultUrlOptions.Language = Language.Parse(Context.Site.SiteInfo.Language);
+        }
+
+        var siteInfo = ResolveTargetSite(item, true);
+        defaultUrlOptions.Language = Language.Parse(siteInfo.Language);
+      }
+    }
+
+    private SiteInfo ResolveTargetSite(Item item, bool resolveSite)
+    {
+      Assert.ArgumentNotNull(item, "item");
+
+      var urlOptions = this.GetDefaultUrlOptions();
+      urlOptions.SiteResolving = resolveSite;
+
+      return this.CreateLinkBuilder(urlOptions).GetTargetSite(item);
+    }
+
+    private static bool MatchCurrentSite(Item item, SiteContext currentSite)
+    {
+      Assert.ArgumentNotNull(item, "item");
+      Assert.ArgumentNotNull(currentSite, "currentSite");
+
+      if (!Settings.Rendering.SiteResolvingMatchCurrentSite)
+      {
+        return false;
+      }
+
+      if (Settings.Rendering.SiteResolvingMatchCurrentLanguage && !item.Language.ToString().Equals(currentSite.Language, StringComparison.InvariantCultureIgnoreCase))
+      {
+        return false;
+      }
+
+      var fullPath = item.Paths.FullPath;
+      var startPath = currentSite.StartPath;
+
+      if (!fullPath.StartsWith(startPath, StringComparison.InvariantCultureIgnoreCase))
+      {
+        return false;
+      }
+
+      return (fullPath.Length <= startPath.Length) || (fullPath[startPath.Length] == '/');
+    }
+
+    #endregion Private Methods
+  }
+}
---
+++ src/Sitecore.Support.116889/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.116889")]
+[assembly: AssemblyProduct("Sitecore.Support.116889")]
+[assembly: AssemblyVersion("8.1.1.0")]
+[assembly: AssemblyFileVersion("8.1.1.0")]
+[assembly: AssemblyInformationalVersion("8.1.1 rev. 160818")]
+
+[assembly: AssemblyDescription("Sitecore Support Patch")]
+[assembly: AssemblyCompany("Sitecore Corporation")]
+[assembly: AssemblyCopyright("Copyright © 2016 Sitecore Corporation")]
+[assembly: AssemblyTrademark("Sitecore® is a registered trademark of Sitecore Corporation")]
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("54b1c085-229d-4c0d-a370-fb4645ea6404")]
